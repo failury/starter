9115ebea1d661cbd8e85085a903b7e00767b2c3f
failury <failury29@mgail.com>
2026-02-25 18:43:06 +0000

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+if vim.fn.executable('zsh') == 1 then
+  vim.opt.shell = 'zsh'
+  vim.opt.shellcmdflag = '-lc'
+end
